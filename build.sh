00651ea5d43157d2f5ecc1828d3be4cfd40e3c55
Li Yi <denverdino@gmail.com>
2017-08-05 10:59:13 +0800

    Update to latest containers for K8S 1.7
    Change-Id: I3bd701970375c2b2919c7f5d579a0dd85889ff14

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ set -e
 IMAGES="flannel etcd k8s-dns-kube-dns k8s-dns-dnsmasq-nanny k8s-dns-sidecar kubedns exechealthz kubernetes-dashboard pause kube-addon-manager kube-dnsmasq"
 arch=amd64
 
-function main()
+function sync_images_with_arch ()
 {
     for img in $IMAGES;
     do
@@ -29,10 +29,44 @@ function main()
             fi
             echo "generate Dockerfile: $path/Dockerfile,  content:FROM gcr.io/google_containers/$img-$arch:$tag"
             echo "FROM gcr.io/google_containers/$img-$arch:$tag" > $path/Dockerfile
-            #docker build -t registry.cn-hangzhou.aliyuncs.com/google-containers/$img-$arch:$tag -f $path/Dockerfile .
-            #docker push registry.cn-hangzhou.aliyuncs.com/google-containers/$img-$arch:$tag
+            #docker build -t registry.cn-hangzhou.aliyuncs.com/google_containers/$img-$arch:$tag -f $path/Dockerfile .
+            #docker push registry.cn-hangzhou.aliyuncs.com/google_containers/$img-$arch:$tag
         done
         echo .
     done
 }
-main
+
+
+IMAGES2="kube-cross echoserver heapster heapster_influxdb heapster_grafana defaultbackend nginx-ingress-controller"
+
+function sync_images ()
+{
+    for img in $IMAGES2;
+    do
+        echo "======================================= image:[$img] ============================================"
+        tags=$(curl -k -s -X GET https://gcr.io/v2/google_containers/$img/tags/list | jq -r '.tags[]'|sort -r)
+        if [ $? -ne 0 ];then
+            curl -k -s -X GET https://gcr.io/v2/google_containers/$img/tags/list | jq -r '.tags[]'|sort -r
+            echo "error occurred while get tags."
+            exit
+        fi
+
+        for tag in $tags
+        do
+            path=$img/$tag
+            mkdir -p $path
+            if [ -f $path/Dockerfile ] ;then
+                echo "[ Image: $img, Tag: $tag ] already exist [$path/Dockerfile], skip!"
+                continue
+            fi
+            echo "generate Dockerfile: $path/Dockerfile,  content:FROM gcr.io/google_containers/$img:$tag"
+            echo "FROM gcr.io/google_containers/$img:$tag" > $path/Dockerfile
+            #docker build -t registry.cn-hangzhou.aliyuncs.com/google_containers/$img:$tag -f $path/Dockerfile .
+            #docker push registry.cn-hangzhou.aliyuncs.com/google_containers/$img:$tag
+        done
+        echo .
+    done
+}
+sync_images_with_arch
+sync_images
+
